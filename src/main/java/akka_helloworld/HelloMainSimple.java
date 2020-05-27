2907bd6c927ce1d81d2023d34cf3f42f84cf9b08
Freejourney <983621131@qq.com>
2020-05-27 18:36:04 +0800

    Update HelloMainSimple.java

--- src/main/java/akka_helloworld/HelloMainSimple.java
+++ src/main/java/akka_helloworld/HelloMainSimple.java
@@ -5,7 +5,18 @@ import akka.actor.ActorSystem;
 import akka.actor.Props;
 import com.typesafe.config.ConfigFactory;
 
-
+/*
+ * 程序的执行流程如下：
+ *      1. 在该main方法中创建ActorSystem。ActorSystem是一个用来管理和维护Actor的系统，
+ *         ActorSystem的名字对应于一个目录，在这里是HelloActorSystem, 在这个目录下又对
+ *         应着更细小的目录。
+ *               如这里的HelloWorldActor目录为：akka://HelloActorSystem/user/helloworldActor/
+ *      2. 然后创建了Actor引用，实例化了HelloWorldActor对象并给其取名helloworldActor
+ *      3. 在helloworldActor启动之前会执行它的preStart方法。在preStart()方法中又创建了GreeterActor
+ *         对象，并向其发送了消息。
+ *      4. GreeterActor收到消息后执行Receive()方法，在方法体中又发送消息回发来消息的Actor
+ *      5. helloworldActor收到消息后再次向GreeterActor发送消息并stop掉了自己
+ */
 public class HelloMainSimple {
 
     public static void main(String[] args) {
