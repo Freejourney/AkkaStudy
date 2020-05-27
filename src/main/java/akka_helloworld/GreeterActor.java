c6e09fd0e4a8e1d460712a6771c95b474cebb216
Freejourney <983621131@qq.com>
2020-05-27 18:23:01 +0800

    Update GreeterActor.java

--- src/main/java/akka_helloworld/GreeterActor.java
+++ src/main/java/akka_helloworld/GreeterActor.java
@@ -3,7 +3,11 @@ package akka_helloworld;
 import akka.actor.UntypedAbstractActor;
 
 
-
+/*
+ * Akka中的Actor基于收到的消息来运作。
+ * 简单来说，Actor就好像处于一种被动的状态，需要收到别的Actor
+ * 发来的消息它才工作，它在工作的时候也可以给其它的Actor发送消息。
+ */
 public class GreeterActor extends UntypedAbstractActor {
 
     public static enum Msg {
@@ -14,7 +18,7 @@ public class GreeterActor extends UntypedAbstractActor {
     public void onReceive(Object message) throws Throwable {
         if (message == Msg.GREET) {
             System.out.println("Hello World");
-            getSender().tell(Msg.DONE, getSelf());
+            getSender().tell(Msg.DONE, getSelf());  // tell()将消息发送给发来消息的Actor，消息的类型是Object类型
         } else {
             unhandled(message);
         }
